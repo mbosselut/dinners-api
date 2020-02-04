b1d443b48d975b2765bb581a250ed6859d5399d3
mbosselut <duchmol7@hotmail.com>
2020-02-04 16:24:38 +0100

    Create GET and POST dinner routes

--- dinner/model.js
+++ dinner/model.js
@@ -4,6 +4,10 @@ const sequelize = require('../db');
 const Dinner = sequelize.define(
   'dinner',
   {
+    date: {
+      type: Sequelize.DATEONLY,
+      allowNull: false
+    },
     meal: {
       type: Sequelize.STRING,
       allowNull: false
--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const jsonMiddleware = bodyParser.json();
 const cors = require('cors');
 const corsMiddleware = cors();
 const port = 4000;
-const User = require('./dinner/model');
+const Dinner = require('./dinner/model');
 
 app.use(corsMiddleware);
 app.use(jsonMiddleware);
+
+app.get('/dinner', (req, res, next) => {
+  Dinner.findAll()
+    .then(dinners => res.json(dinners))
+    .catch(next);
+});
+
+app.post('/dinner', (req, res, next) => {
+  console.log(req.body);
+  Dinner.create(req.body);
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
