fb15a58933fc41ee784c5d205119182681cfd68d
mbosselut <duchmol7@hotmail.com>
2020-02-04 16:30:57 +0100

    Add DELETE dinner route

--- index.js
+++ index.js
@@ -21,4 +21,11 @@ app.post('/dinner', (req, res, next) => {
   Dinner.create(req.body);
 });
 
+app.delete('/dinner/:dinnerId', (req, res, next) => {
+  Dinner.findOne({ where: { id: parseInt(req.params.dinnerId) } })
+    .then(dinner => dinner.destroy())
+    .then(() => res.json(`Dinner with id ${req.params.dinnerId} was deleted`))
+    .catch(next);
+});
+
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
